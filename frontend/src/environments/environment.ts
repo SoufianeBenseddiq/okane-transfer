5b87b731c4c75f469263833fbe4c1983b7d1d5c4
Soufiane Benseddiq <soufianebenseddiq8@gmail.com>
2026-06-16 10:13:26 +0100

    fix server cors config

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,3 +2,7 @@ export const environment = {
   production: true,
   apiUrl: 'http://13.60.74.176:8080/okane_transfer_war'
 };
+// export const environment = {
+//   production: true,
+//   apiUrl: 'http://localhost:8080/okane_transfer_war'
+// };
