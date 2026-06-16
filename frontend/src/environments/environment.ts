2ed980ec767d335e68333a44011da0fd4c747bbb
Abdelghani <abdelghaniben40@gmail.com>
2026-06-16 04:04:34 +0100

    updated apiUrl for frontend environment for production

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // apiUrl: 'http://13.60.74.176:8080/okane_transfer_war'
-  apiUrl: 'http://localhost:8080/okane_transfer_war'
+  apiUrl: 'http://13.60.74.176:8080/okane_transfer_war'
 };
